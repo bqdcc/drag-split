bcb0d6593a1e1fb7f2bbd2510fd86268c546e083
聪明的匆匆 <bqdcc@outlook.com>
2024-01-23 23:56:15 +0800

    ✨ feat: add resize and panel merge

--- script.js
+++ script.js
@@ -2,48 +2,81 @@ window.Split(["#split-0", "#split-1"], {
   sizes: [75, 25],
 });
 
-const draggables = document.querySelectorAll(".draggable");
-const containers = document.querySelectorAll(".container");
+const snapThreshold = 10;
+const contentEl = document.querySelector(".content");
+function onStart(el, x, y) {
+  el.style.top = el.offsetTop + "px";
+  el.style.bottom = "auto";
+}
+function onEnd(el, x, y) {
+  if (
+    el.parentNode.scrollHeight - (el.offsetTop + el.offsetHeight) <
+    snapThreshold
+  ) {
+    el.style.top = "auto";
+    el.style.bottom = "0px";
+  }
+  if (
+    el.parentNode.scrollWidth - (el.offsetLeft + el.offsetWidth) <
+    snapThreshold
+  ) {
+    el.style.left = "auto";
+    el.style.right = "0px";
+    el.remove();
+  }
+  if (el.offsetTop < snapThreshold) {
+    el.style.top = "0px";
+  }
+  if (el.offsetLeft < snapThreshold) {
+    el.style.left = "0px";
+  }
+}
 
-draggables.forEach((draggable) => {
-  draggable.addEventListener("dragstart", () => {
-    draggable.classList.add("dragging");
+const tools = document.querySelectorAll(".tools .tool");
+
+tools.forEach((tool) => {
+  tool.addEventListener("dragstart", () => {
+    tool.classList.add("dragging");
   });
 
-  draggable.addEventListener("dragend", () => {
-    draggable.classList.remove("dragging");
-  });
-});
-
-containers.forEach((container) => {
-  container.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    const afterElement = getDragAfterElement(container, e.clientY);
-    const draggable = document.querySelector(".dragging");
-    if (afterElement == null) {
-      container.appendChild(draggable);
+  tool.addEventListener("dragend", (e) => {
+    const inElement = getDragInElement(contentEl, e.clientX, e.clientY);
+    console.log(inElement);
+    if (inElement != null) {
+      const dupNode = document.createElement("p");
+      dupNode.classList.add("panel");
+      dupNode.style.backgroundColor = e.target.attributes["bg-color"].value;
+      dupNode.style.left = e.clientX - inElement.style.left + "px";
+      dupNode.style.top = e.clientY - inElement.style.top + "px";
+      inElement.appendChild(dupNode);
+      resizedrag(dupNode, dupNode, onStart, onEnd);
     } else {
-      container.insertBefore(draggable, afterElement);
     }
+    tool.classList.remove("dragging");
   });
 });
 
-function getDragAfterElement(container, y) {
-  console.log(y);
+function getDragInElement(contentEl, x, y) {
   const draggableElements = [
-    ...container.querySelectorAll(".draggable:not(.dragging)"),
+    contentEl,
+    ...contentEl.querySelectorAll(".panel"),
   ];
-
   return draggableElements.reduce(
     (closest, child) => {
       const box = child.getBoundingClientRect();
-      const offset = y - box.top - box.height / 2;
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child };
+      const offsetX = x - box.left;
+      const offsetY = y - box.top;
+      if (
+        offsetX > 0 &&
+        offsetX < box.right &&
+        offsetY > 0 &&
+        offsetY < box.bottom
+      ) {
+        return { offsetX: offsetX, offsetY: offsetY, element: child };
       } else {
         return closest;
       }
     },
-    { offset: Number.NEGATIVE_INFINITY }
+    { offsetX: Number.NEGATIVE_INFINITY, offsetY: Number.NEGATIVE_INFINITY }
   ).element;
 }
